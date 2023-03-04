4dbcfe1a6e0f1fa1e8a326b97f8b9ca07178b84a
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-04 13:12:44 +0200

    added export

--- rodoznanie-client/src/app/history/history.module.ts
+++ rodoznanie-client/src/app/history/history.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddHistoryComponent } from './add-history/add-history.component';
 
-
-
 @NgModule({
-  declarations: [
-    AddHistoryComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AddHistoryComponent],
+  imports: [CommonModule],
+  exports: [AddHistoryComponent],
 })
-export class HistoryModule { }
+export class HistoryModule {}
