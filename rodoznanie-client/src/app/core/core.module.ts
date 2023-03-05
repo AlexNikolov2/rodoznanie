6574d95793c573d99af3c9ae964898740b02f0fc
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-05 20:24:59 +0200

    added landing page

--- rodoznanie-client/src/app/core/core.module.ts
+++ rodoznanie-client/src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, LandingPageComponent],
   imports: [CommonModule],
   exports: [HeaderComponent],
 })
