2627bc32642cde8250609fbf2a8e0a1996e53924
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-06 16:10:07 +0200

    landing page looks great!

--- rodoznanie-client/src/app/core/core.module.ts
+++ rodoznanie-client/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HeaderComponent, LandingPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [HeaderComponent, LandingPageComponent],
 })
 export class CoreModule {}
--- rodoznanie-client/src/app/core/landing-page/landing-page.component.ts
+++ rodoznanie-client/src/app/core/landing-page/landing-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
-
+  icons = {
+    globe: faGlobe,
+  };
+  constructor() {}
 }
