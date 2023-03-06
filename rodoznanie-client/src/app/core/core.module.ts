ffec00ae5b1ff8dbec4099185bfc0b27414a3d53
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-06 16:10:34 +0200

    added footer

--- rodoznanie-client/src/app/core/core.module.ts
+++ rodoznanie-client/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, LandingPageComponent],
+  declarations: [HeaderComponent, LandingPageComponent, FooterComponent],
   imports: [CommonModule, FontAwesomeModule],
   exports: [HeaderComponent, LandingPageComponent],
 })
