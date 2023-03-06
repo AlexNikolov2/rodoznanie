27a6e9a797241eafe038cbaf2fc374f5acf3bd5a
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-06 22:25:00 +0200

    app

--- rodoznanie-client/src/app/app.module.ts
+++ rodoznanie-client/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { HistoryModule } from './history/history.module';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 @NgModule({
   declarations: [AppComponent],
