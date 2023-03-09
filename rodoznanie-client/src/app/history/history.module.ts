cf660f409e2a01f0a1e3427ad55210fc6415a011
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-09 18:02:17 +0200

    map component

--- rodoznanie-client/src/app/history/history.module.ts
+++ rodoznanie-client/src/app/history/history.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddHistoryComponent } from './add-history/add-history.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [AddHistoryComponent],
+  declarations: [AddHistoryComponent, MapComponent],
   imports: [CommonModule],
   exports: [AddHistoryComponent],
 })
