c1ff5bf628d0935b7f3d68314b176e7a9a5d25b6
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-14 00:56:17 +0200

    working on google maps

--- rodoznanie-client/src/app/history/history.module.ts
+++ rodoznanie-client/src/app/history/history.module.ts
@@ -4,10 +4,11 @@ import { AddHistoryComponent } from './add-history/add-history.component';
 import { MapComponent } from './map/map.component';
 import { HistoryDetailsComponent } from './history-details/history-details.component';
 import { HistoryRoutingModule } from './history-routing.module';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [AddHistoryComponent, MapComponent, HistoryDetailsComponent],
-  imports: [CommonModule, HistoryRoutingModule],
+  imports: [CommonModule, HistoryRoutingModule, GoogleMapsModule],
   exports: [AddHistoryComponent],
 })
 export class HistoryModule {}
