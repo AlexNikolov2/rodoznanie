62208b26be1ded6bd7f98e79431be5c587b26bd9
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-16 23:20:55 +0200

    functions defined

--- rodoznanie-client/src/app/history/history.service.ts
+++ rodoznanie-client/src/app/history/history.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HistoryService {
-  constructor() {}
+  constructor(private firestore: Firestore) {}
 
   //get all stories
+  getAllStories() {}
   //get story by id
+  getStoryById() {}
   //create story
+  createStory() {}
   //edit story
+  editStory() {}
   //delete story
+  deleteStory() {}
   //relate to a story*
 }
