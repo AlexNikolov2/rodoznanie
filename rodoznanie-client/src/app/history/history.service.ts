e3f25ece818aee1b64778da6949e39e80639a2c5
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-16 20:59:24 +0200

    defined needed crud functions

--- rodoznanie-client/src/app/history/history.service.ts
+++ rodoznanie-client/src/app/history/history.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HistoryService {
+  constructor() {}
 
-  constructor() { }
+  //get all stories
+  //get story by id
+  //create story
+  //edit story
+  //delete story
+  //relate to a story*
 }
