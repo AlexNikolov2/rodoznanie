904052e610b015844222382856d31471e447a302
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-16 22:43:05 +0200

    added more props

--- rodoznanie-client/src/app/shared/interfaces/story.ts
+++ rodoznanie-client/src/app/shared/interfaces/story.ts
@@ -1,7 +1,12 @@
+import { User } from 'firebase/auth';
+
 export interface Story {
   id: string;
   name: string;
   address: string;
   history: string;
   image: string;
+  relatives: string[];
+  userId: string | null;
+  user: User;
 }
