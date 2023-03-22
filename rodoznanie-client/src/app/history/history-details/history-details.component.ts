fcf1da3d97fd04509a5e60417d3afe42fc0b0235
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-23 01:08:41 +0200

    started work on details

--- rodoznanie-client/src/app/history/history-details/history-details.component.ts
+++ rodoznanie-client/src/app/history/history-details/history-details.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { HistoryService } from '../history.service';
 
 @Component({
   selector: 'app-history-details',
   templateUrl: './history-details.component.html',
-  styleUrls: ['./history-details.component.scss']
+  styleUrls: ['./history-details.component.scss'],
 })
 export class HistoryDetailsComponent {
+  story: any;
+  postId!: any;
+  currUser?: string | null;
 
+  constructor(
+    public historyService: HistoryService,
+    public route: ActivatedRoute,
+    public router: Router,
+    public auth: AngularFireAuth,
+    public authService: AuthService
+  ) {}
 }
