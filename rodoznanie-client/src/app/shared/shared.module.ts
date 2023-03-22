d203e6e73b1637b1d712931d006abbdafece737e
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-23 01:17:15 +0200

    shorten pipe

--- rodoznanie-client/src/app/shared/shared.module.ts
+++ rodoznanie-client/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ShortenPipe } from './shorten.pipe';
 
 
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule
