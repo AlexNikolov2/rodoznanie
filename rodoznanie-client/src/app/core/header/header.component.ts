b98bcaffb6354e5ea8f10f564b3004bdbf2b6ece
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-23 19:36:22 +0200

    auth guard

--- rodoznanie-client/src/app/core/header/header.component.ts
+++ rodoznanie-client/src/app/core/header/header.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  style,
+  trigger,
+  state,
+  transition,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 import { faEarthEurope } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from 'src/app/auth/auth.service';
@@ -6,6 +13,15 @@ import { AuthService } from 'src/app/auth/auth.service';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(90deg)' })),
+      transition('rotated => default', animate('150ms ease-out')),
+      transition('default => rotated', animate('150ms ease-in')),
+    ]),
+  ],
 })
 export class HeaderComponent {
   icons = {
@@ -13,4 +29,29 @@ export class HeaderComponent {
   };
 
   constructor(public authService: AuthService) {}
+
+  toggleHamburger() {
+    this.rotate();
+
+    let navbar = document.querySelector<HTMLElement>('#navbar');
+    let header = document.querySelector<HTMLElement>('#header');
+
+    if (navbar!.style.display === 'flex') {
+      navbar!.style.display = 'none';
+      header!.style.flexDirection = 'row';
+    } else {
+      navbar!.style.display = 'flex';
+      navbar!.style.flexDirection = 'column';
+      header!.style.flexDirection = 'column';
+    }
+    if (screen.width > 1080) {
+      navbar!.style.display = 'flex';
+      navbar!.style.flexDirection = 'row';
+    }
+  }
+
+  state: string = 'default';
+  rotate() {
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+  }
 }
