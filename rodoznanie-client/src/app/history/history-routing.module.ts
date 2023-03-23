53d12a8b94f9a8992c73e8f5705aa700965905f8
Aleks Nikolov <tcr.official.world@gmail.com>
2023-03-23 16:39:43 +0200

    auth guard

--- rodoznanie-client/src/app/history/history-routing.module.ts
+++ rodoznanie-client/src/app/history/history-routing.module.ts
@@ -4,12 +4,28 @@ import { MapComponent } from './map/map.component';
 import { AddHistoryComponent } from './add-history/add-history.component';
 import { HistoryDetailsComponent } from './history-details/history-details.component';
 import { EditHistoryComponent } from './edit-history/edit-history.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
-  { path: 'add-history', component: AddHistoryComponent },
-  { path: 'history-details/:id', component: HistoryDetailsComponent },
-  { path: 'edit/:id', component: EditHistoryComponent },
+  {
+    path: 'add-history',
+    component: AddHistoryComponent,
+    canActivate: [AuthGuard],
+    data: { authRequired: true },
+  },
+  {
+    path: 'history-details/:id',
+    component: HistoryDetailsComponent,
+    canActivate: [AuthGuard],
+    data: { authRequired: true },
+  },
+  {
+    path: 'edit/:id',
+    component: EditHistoryComponent,
+    canActivate: [AuthGuard],
+    data: { authRequired: true },
+  },
 ];
 
 @NgModule({
